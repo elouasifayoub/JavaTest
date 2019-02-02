6de7f0a92cd04c4332f69e061faa3b9f2d9bf605
‎أسامة الادريسي‎ <gauss.seidel.fr@gmail.com>
2019-02-02 17:11:10 +0000

    Update TabChar

--- TabChar.java
+++ TabChar.java
@@ -2,8 +2,8 @@
     {
             public static void main(String args[]) 
             {    
-                char tab [] = new char[10];
-                for (int i = 0; i < 10; ++i) {
+                char tab [] = new char[20];
+                for (int i = 0; i < 20; ++i) {
                     tab[i] = 'i';
                     System.out.print(tab[i] + "" );
                     i++;
