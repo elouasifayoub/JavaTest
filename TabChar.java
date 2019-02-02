6ac6cbd6122e988d416c6029e667a098633204aa
elidrissi.ousama.ma@gmail.com <elidrissi.ousama.ma@gmail.com>
2019-02-02 18:06:12 +0100

    Initial commit

---
+++ TabChar.java
@@ -0,0 +1,12 @@
+    public class TabChar  
+    {
+            public static void main(String args[]) 
+            {    
+                char tab [] = new char[10];
+                for (int i = 0; i < 10; ++i) {
+                    tab[i] = 'i';
+                    System.out.print(tab[i] + "" );
+                    i++;
+                }
+            } 
+        }
---
+++ TheTimeInWords.java
@@ -0,0 +1,52 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the timeInWords function below.
+    static String timeInWords(int h, int m) {
+        String ones[] = {" ", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "tweleve"};
+        String minutes[] = {" ", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "tweleve", "thirteen", "fourteen", "quarter", "sixteen", "seventeen", "eighteen", "nineteen", "twenty", "twenty-one", "twenty-two", "twenty-three", "twenty-four", "twenty-five", "twenty-six", "twenty-seven", "twenty-eight", "twenty-nine", " half"};
+        String time;
+        if(m > 30) {
+            time = ones[h+1];
+        } else {
+            time = ones[h];
+        }
+        if(m == 0) {
+            time = time + " o' clock";
+        } else if (m >= 1 && m <= 30) {
+            if(m == 1) {
+                
+            }
+        }
+        return time;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int h = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int m = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        String result = timeInWords(h, m);
+
+        bufferedWriter.write(result);
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
+
